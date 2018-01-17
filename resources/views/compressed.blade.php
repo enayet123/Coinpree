0506f9ec6450d5d2c7b71ea4ed3d88ea86c6e69a
Enayet Hussain <enayet123@hotmail.com>
2018-01-17 20:39:38 +0000

    Google Analytics Tracking

--- resources/views/compressed.blade.php
+++ resources/views/compressed.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63150348-2"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date());gtag("config","UA-63150348-2");</script>
 <meta charset=utf-8>
 <meta http-equiv=X-UA-Compatible content="IE=edge">
 <meta charset=UTF-8>
